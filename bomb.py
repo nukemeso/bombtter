359ec4f875e74e83ae06421ef533fa4e9baa6b02
nukemeso <poppo0505920@yahoo.co.jp>
2016-10-28 12:57:35 +0900

    ふぁぼ
    ふぁぼ

--- bomb.py
+++ bomb.py
@@ -13,16 +13,18 @@ AS = ''# Accesss Token Secert
 
 url = "https://api.twitter.com/1.1/search/tweets.json?"
 urlpost = "https://api.twitter.com/1.1/statuses/update.json"
+favurl = "https://api.twitter.com/1.1/favorites/create.json"
 params ={"q":"爆発しろ exclude:retweets","result_type": "recent","count":100}
 twitter = OAuth1Session(CK, CS, AT, AS)
 responce = twitter.get(url, params = params)
-loop =0
+idlist=""
 data=""
 if responce.status_code == 200:
  SEARCH=json.loads(responce.text)
  for a in SEARCH["statuses"]:
   if(loop==0):
    via=a["source"]
+   favid=a["id"]
    H=a["text"]
    B=int(a["text"].find("爆発しろ"))
    A=int(len(a["text"]))
@@ -30,14 +32,21 @@ if responce.status_code == 200:
      if not(("定期" in H)or("\n" in H)or("pic." in H) or ("@" in H) or("#" in H)) and (A<80)and(A>8) and not(("bot" in via) or ("twirobo" in via) or("auto" in via) or("地震" in via)or("BOT" in via)or("定期" in via)or("IFTTT" in via)):
       try:
        data+=H+","
+       islist+=favid+","
        if("cranky" in H)or("Cranky" in H)or("コナミ社長" in H)or("KONAMI社長" in H):
         data+=H+","
+        islist+=favid+","
       except UnicodeEncodeError:
        pass
+ idlist=idlist.rstrip(",")
+ List=idlist.split(",")
  data=data.rstrip(",")
  ANS=data.split(",")
- TWEET=ANS[random.randint(0,len(ANS)-1)]
+ AAAA=random.randint(0,len(ANS)-1)
+ TWEET=ANS[AAAA]
+ FAVID=List[AAAA]
  RE=TWEET.replace("爆発しろ","が爆発しました")
  postparams={"status":RE}
  ZZZ=twitter.post(urlpost, params = postparams)
- if
+ favparams={"id":FAVID}
+ FAVEXE=twitter.post(favurl,params=favparams)
