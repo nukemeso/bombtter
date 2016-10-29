f865045a83cee5ff1025d40a7b0702f7cfe03802
nukemeso <poppo0505920@yahoo.co.jp>
2016-10-29 23:59:01 +0900

    俺爆発しろとかに対応する機能は削除、同じツイートを拾ってpostに失敗したとき三回だけ拾うツイートを選び直す様に変更。

--- bomb.py
+++ bomb.py
@@ -12,6 +12,12 @@ CK = '' #Consumer Key
 CS = '' # Consumer Secret
 AT = '' # Access Token
 AS = ''# Accesss Token Secert
+
+CK = 'JwuvR50gm2WTwT3Lp5KkAMj85' #Consumer Key	
+CS = 'dDNEAcqDOvYGa8ujBJfhnoDbF2TpFrnN5ijJ92gKODtQVvjdIi' # Consumer Secret
+AT = '815236832-vxFnjhzO1ofKhHlC0viH202pLCL5IfsSJxacLYZr' # Access Token
+AS = 'mSdeSjSHKnbYrhDI0bSmaB7gcPy1CbeFnC7r5VhKn55Ea'# Accesss Token Secert
+
  
 url = "https://api.twitter.com/1.1/search/tweets.json?"
 urlpost = "https://api.twitter.com/1.1/statuses/update.json"
@@ -19,15 +25,13 @@ favurl = "https://api.twitter.com/1.1/favorites/create.json"
 params ={"q":"爆発しろ exclude:retweets","result_type": "recent","count":100}
 twitter = OAuth1Session(CK, CS, AT, AS) #認証
 responce = twitter.get(url, params = params)#爆発しろを含むツイートを100検索
-namelist=""#この中にツイート主の@抜きIDを保存
 idlist=""#この中にツイートIDを入れていきます。
 data=""#この中にツイート本文を入れていきます。
 if responce.status_code == 200:
  SEARCH=json.loads(responce.text)
  for a in SEARCH["statuses"]:
-   scrname = a["user"]["screen_name"]#ツイート主の判定
    via=a["source"] #viaの判定
-   favid=a["id"]#ツイートIDの取得
+   favid=str(a["id"])#ツイートIDの取得
    H=a["text"]#ツイート本文の取得
    B=int(a["text"].find("爆発しろ"))
    A=int(len(a["text"]))
@@ -35,14 +39,12 @@ if responce.status_code == 200:
      if not(("定期" in H)or("\n" in H)or("pic." in H) or ("@" in H) or("#" in H)) and (A<=80)and(A>8) and not(("bot" in via) or ("twirobo" in via) or("auto" in via) or("地震" in via)or("BOT" in via)or("定期" in via)or("IFTTT" in via)):
       #9～80文字でリプライ、ハッシュタグ、改行、画像がない、且つbotで無いと思われるツイートを取得
       try:
-       data+=H+","
-       idlist+=str(favid)+","
-       namelist+=scrname+","#リストの中に入れていきます。
+       idlist+=favid+","
+       data+=H+","#リストの中に入れていきます。
        if("cranky" in H)or("Cranky" in H)or("コナミ社長" in H)or("KONAMI社長" in H):
         #crankyとKONAMI社長は確率UP
+        idlist+=favid+","
         data+=H+","
-        idlist+=str(favid)+","
-        namelist+=scrname+","
       except UnicodeEncodeError:
        pass
  #↓ツイートIDと本文をリスト化します。
@@ -50,33 +52,26 @@ if responce.status_code == 200:
  ID=idlist.split(",")
  data=data.rstrip(",")
  ANS=data.split(",")
- namelist=namelist.rstrip(",")
- IDs=namelist.split(",")
 #ツイートの取得はここまで
- 
-if not(ANS[0]==""):
- AAAA=random.randint(0,len(ANS)-1)
- SCRNAME=IDs[AAAA]
- TWEET=ANS[AAAA]
- FAVID=ID[AAAA]#リストとして拾ったツイートの中からランダムで選びます
- if("めかろいど爆発しろ" in TWEET):#ぼむったーっぽく自爆してもらいます
-  RE=TWEET.replace("爆発しろ","が自爆しました")
- elif("から爆発しろ" in TWEET):#～(だ)から爆発しろを自然に爆発させます。
-  RE=TWEET.replace("爆発しろ","爆発しました")
- elif("俺爆発しろ" in TWEET):#俺爆発しろとか言ってるやつのIDを抜き出してみます。
-  RE=TWEET.replace("俺爆発しろ",SCRNAME+"が爆発しました")
+m=0
+while(m<3):
+ if not(ANS[0]==""):
+  AAAA=random.randint(0,len(ANS)-1)
+  TWEET=ANS[AAAA]
+  FAVID=ID[AAAA]#リストとして拾ったツイートの中からランダムで選びます
+  if("めかろいど爆発しろ" in TWEET):#ぼむったーっぽく自爆してもらいます
+   RE=TWEET.replace("爆発しろ","が自爆しました")
+  else:
+   RE=TWEET.replace("爆発しろ","が爆発しました")
+ elif(ANS[0]==""):#ツイートが引っかからなかったらおとなしく自爆してもらいます。
+  self=["爆発させるものがないから代わりにめかろいどが自爆しました","爆発させるものがないから代わりに開発者のくりいむろいどが爆発しました"]
+  RE=self[random.randint(0,1)]
+ #ここから投稿していきます
+ postparams={"status":RE}
+ ZZZ=twitter.post(urlpost, params = postparams)#投稿
+ favparams={"id":int(FAVID)}
+ FAVEXE=twitter.post(favurl,params=favparams)#元ツイをふぁぼ
+ if (ZZZ.status_code == 200):
+  m=3
  else:
-  RE=TWEET.replace("爆発しろ","が爆発しました")
-elif(ANS[0]==""):#ツイートが引っかからなかったらおとなしく自爆してもらいます。
- self=["爆発させるものがないから代わりにめかろいどが自爆しました","爆発させるものがないから代わりに開発者のくりいむろいどが爆発しました"]
- RE=self[random.randint(0,1)]
-
-#ここから投稿していきます
-postparams={"status":RE}
-ZZZ=twitter.post(urlpost, params = postparams)#投稿
-favparams={"id":int(FAVID)}
-FAVEXE=twitter.post(favurl,params=favparams)#元ツイをふぁぼ
-if not(ZZZ.status_code == 200):
- print("投稿できなかったみたいですよ")
-if not(FAVEXE.status_code == 200):
- print("ふぁぼれなかったみたいですよ")
+  m+=1
