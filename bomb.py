09c2489da3b89f234c2d411e57899bfd59fd8665
nukemeso <poppo0505920@yahoo.co.jp>
2016-11-01 19:38:38 +0900

    1.1.0っぽいなにか
    結局config管理はpyのがやりやすかった。NGワードをコンフィグから設定する(本来の検索オプション使ってるから不格好)、これで@は尋はないと信じる。一応txtファイルのログを吐くようにしたけどいらなかったらしたの部分を消しとけばOK

--- bomb.py
+++ bomb.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-                                                                                                                                            
 
 from requests_oauthlib import OAuth1Session
+import datetime
 import requests
 import json
 import sys
@@ -11,26 +12,28 @@ CK=setting.CK
 CS=setting.CS
 AT=setting.AT
 AS=setting.AS
- 
+excludes=setting.excludes
 url = "https://api.twitter.com/1.1/search/tweets.json?"
 urlpost = "https://api.twitter.com/1.1/statuses/update.json"
 favurl = "https://api.twitter.com/1.1/favorites/create.json"
-params ={"q":"爆発しろ exclude:retweets","result_type": "recent","count":100}
+params ={"q":"爆発しろ exclude:retweets -filter:images -filter:videos -filter:links "+excludes,"result_type": "recent","count":100}
 twitter = OAuth1Session(CK, CS, AT, AS) #認証
 responce = twitter.get(url, params = params)#爆発しろを含むツイートを100検索
 idlist=""#この中にツイートIDを入れていきます。
 data=""#この中にツイート本文を入れていきます。
 FA="false"
+bombed="false"
 if responce.status_code == 200:
  SEARCH=json.loads(responce.text)
  for a in SEARCH["statuses"]:
    via=a["source"] #viaの判定
+   reply=a["in_reply_to_status_id_str"]
    favid=str(a["id"])#ツイートIDの取得
    H=a["text"]#ツイート本文の取得
    B=int(a["text"].find("爆発しろ"))
    A=int(len(a["text"]))
    if(A-B-4==0):#ツイートの長さとかを測定して後方一致のツイートを絞る
-     if not(("定期" in H)or("\n" in H)or("pic." in H) or ("@" in H) or("#" in H)) and (A<=80)and(A>8) and not(("bot" in via) or ("twirobo" in via) or("auto" in via) or("地震" in via)or("BOT" in via)or("定期" in via)or("IFTTT" in via)):
+     if not(("定期" in H)or("\n" in H)or(".@" in H)or("@" in H) or("#" in H)) and (A<=80)and(A>8) and not(("bot" in via) or ("twirobo" in via) or("auto" in via) or("地震" in via)or("BOT" in via)or("定期" in via)or("IFTTT" in via))and not(reply==""):
       #9～80文字でリプライ、ハッシュタグ、改行、画像がない、且つbotで無いと思われるツイートを取得
       try:
        idlist+=favid+","
@@ -55,12 +58,16 @@ while(m<3):
   FAVID=ID[AAAA]#リストとして拾ったツイートの中からランダムで選びます
   if("めかろいど爆発しろ" in TWEET):#ぼむったーっぽく自爆してもらいます
    RE=TWEET.replace("爆発しろ","が自爆しました")
+   bombed="true"
   elif("から爆発しろ" in TWEET):
    RE=TWEET.replace("爆発しろ","爆発しました")#～から爆発しろに対応
+   bombed="true"
   elif("くりいむろいど爆発しろ" in TWEET):
    RE=TWEET.replace("爆発しろ","大爆発しました")#くりいむを大爆発
+   bombed="true"
   else:
    RE=TWEET.replace("爆発しろ","が爆発しました")
+   bombed="true"
  else:#ツイートが引っかからなかったらおとなしく自爆してもらいます。
   self=["爆発させるものがないから代わりにめかろいどが自爆しました","爆発させるものがないから代わりに開発者のくりいむろいどが爆発しました"]
   RE=self[random.randint(0,1)]
@@ -74,7 +81,6 @@ while(m<3):
   FA="true"
  else:
   m+=1
- print(FA)
   
 if(FA=="false"):#三回リトライしてもダメなとき、自爆します
  RAN1=random.randint(1,200)
@@ -85,3 +91,10 @@ if(FA=="false"):#三回リトライしてもダメなとき、自爆します
   RE="同じツイートばっかり拾おうとするからめかろいどが爆発しました"
  postparams={"status":RE}
  ZZZ=twitter.post(urlpost, params = postparams)#投稿
+
+#ここから下はログの出力。消しても動きます
+if(bombed=="true")and(FA=="true"):
+ TIMEs=datetime.datetime.today().strftime("%Y/%m/%d %H:%M:%S")
+ with open("explotion.txt",mode="a",encoding="utf-8") as EX:
+  EX.write(str(TIMEs)+":"+RE+"\n")#時間と一緒にツイートを出力するです
+  EX.close()
--- setting.py
+++ setting.py
@@ -1,5 +1,7 @@
-
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 CK = '' #Consumer Key	
 CS = '' # Consumer Secret
-AT = ''# Access Token
+AT = '' # Access Token
 AS = ''# Accesss Token Secert
+excludes="-広島 -長崎"
